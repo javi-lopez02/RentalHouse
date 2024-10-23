81f42736374178e4caf40e1b260f928c0c529340
javi-lopez02 <mjlphone0210@gmail.com>
2024-10-23 16:08:19 -0400

    retoques

--- js/buyoferts.js
+++ js/buyoferts.js
@@ -1,5 +1,5 @@
 function saveData(divId) {
-    var dataDiv = document.getElementById("dataDiv" + divId);
+    var dataDiv = document.getElementById("dataDiv" + (divId-1));
     var data = dataDiv.innerText || dataDiv.textContent;
     localStorage.setItem("contactDescription", data);
     window.location.href = "contact_us_page.html";
